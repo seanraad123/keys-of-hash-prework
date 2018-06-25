7bba8a2fd59acfeec2d5bb3f77383344f6835f69
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 03:32:28 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -14,9 +14,3 @@ class Hash
     return array
   end
 end
-
-
-
-animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
-
-animals.keys_of("Panama")
