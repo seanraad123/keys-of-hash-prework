0cb101807a8bc0fed9e5c8de62a3fb4b0eb8a86d
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 03:24:54 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -9,5 +9,6 @@ class Hash
   end
 end
 
-{ {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+
+{animals:{"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
   
